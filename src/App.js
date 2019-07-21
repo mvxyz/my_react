ca518bfc68bfddc444c15ff1a36ed3ebbb2c1402
mvxyz <mvxyz@yandex.ru>
2019-07-21 16:21:43 +0300

    До передачи параметров

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Car from './Car/Car'
 
 function App() {
+
+  const divStyle = {
+    textAlign: 'center'
+  }
+
   return (
-    <div className="App">
-    <h1>Здравствуй Мир!</h1>
+    <div style={divStyle}>
+      <h1 style={{ color: 'blue', fontSize: '50px' }}>Здравствуй Мир!</h1>
+      <Car />
+      <Car />
+      <Car />
     </div>
   );
 }
---
+++ src/Car/Car.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        This is car component
+        <p><strong>{Math.round(Math.random()*100)}</strong></p>
+    </div>
+    );
