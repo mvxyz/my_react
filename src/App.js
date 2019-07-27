d381c2a4adc2f6305db4075005f0b5060c1a1285
mvxyz <mvxyz@yandex.ru>
2019-07-27 17:25:43 +0300

    Работа с условными операторами

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Car from './Car/Car'
 
-function App() {
+class App extends Component {
 
-  const divStyle = {
-    textAlign: 'center'
-  }
+ state = {
+ cars: [
+ { name: 'Ford', year: 2018 },
+ { name: 'Audi', year: 2017 },
+ { name: 'Mazda 1', year: 2015 }
+ ],
+ pageTitle: 'React component',
+ showCars: false
+ }
 
-  return (
-    <div style={divStyle}>
-      <h1 style={{ color: 'blue', fontSize: '50px' }}>Здравствуй Мир!</h1>
-      <Car name={'Ford'} year={2018}>
-        <p style={{color:'Red'}}>COLOR</p>
-      </Car>
-      <Car name={'Audi'} year={2017} />
-      <Car name={'Mazda'} year={2015} />
-    </div>
-  );
+ changeTitleHandler = (newTitle) => {
+ this.setState({
+ pageTitle: newTitle
+ })
+ }
+
+ toggleCarsHandler = () => {
+ this.setState({
+ showCars: !this.state.showCars
+ })
+ }
+
+ render() {
+
+ const divStyle = {
+ textAlign: 'center'
+ }
+
+ let cars = null
+
+ if (this.state.showCars) {
+ cars = this.state.cars.map((car, index) => {
+ return (
+ <Car
+ key={index}
+ name={car.name}
+ year={car.year}
+ onChangeTitle={() => this.changeTitleHandler(car.name)}
+ />
+ )
+ })
+ }
+
+ return (
+ <div style={divStyle}>
+ <h1 style={{ color: 'blue', fontSize: '50px' }}>{this.state.pageTitle}</h1>
+
+ <button onClick={this.toggleCarsHandler}>
+ Toggle cars
+ </button>
+
+ { cars }
+
+ </div>
+ );
+ }
 }
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-export default props => (
-    <div>
-        <h1>Car Name: {props.name}</h1>
-        <p>Year: <strong>{props.year}</strong></p>
-    </div>
+export default (props) => (
+ <div>
+ <h1>Car Name: {props.name}</h1>
+ <p>Year: <strong>{props.year}</strong></p>
+ <button onClick = {props.onChangeTitle}>Chanch</button>
+ </div>
 );
