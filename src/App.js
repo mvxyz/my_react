b8cf8b80a9984226311afe627d4a25afa7249709
mvxyz <mvxyz@yandex.ru>
2019-08-01 16:36:21 +0300

    Инициализация State

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Car from './Car/Car'
 
 class App extends Component {
 
-  state = {
-    cars: [
-      { name: 'Ford', year: 2018 },
-      { name: 'Audi', year: 2017 },
-      { name: 'Mazda 1', year: 2015 }
-    ],
-    pageTitle: 'React component',
-    showCars: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      cars: [
+        { name: 'Ford', year: 2018 },
+        { name: 'Audi', year: 2017 },
+        { name: 'Mazda 1', year: 2015 }
+      ],
+      pageTitle: 'React component',
+      showCars: false
+    }
   }
 
   changeTitleHandler = (newTitle) => {
