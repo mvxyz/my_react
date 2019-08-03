c1bb1fd847bb278d878669d08bfc69dae80fbf30
mvxyz <mvxyz@yandex.ru>
2019-08-03 14:06:42 +0300

    Создание Stateful компонента

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
-export default (props) => (
-    <div style={{
-        border:'1px solid #ccc',
-        marginBottom: '10px',
-        display: 'block',
-        padding: '10px'
-    }}>
- <h1>Car Name: {props.name}</h1>
-    <p>Year: <strong>{props.year}</strong></p>
-    <input type='text' onChange={props.onChangeName} value={props.name}></input>
-    <button onClick={props.onDelete}>Delete</button>
- </div >
-);
+class Car extends React.Component {
+    render() {
+        return (
+            <div style={{
+                border: '1px solid #ccc',
+                marginBottom: '10px',
+                display: 'block',
+                padding: '10px'
+            }}>
+                <h1>Car Name: {this.props.name}</h1>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input type='text' onChange={this.props.onChangeName} value={this.props.name}></input>
+                <button onClick={this.props.onDelete}>Delete</button>
+            </div >
+        )
+    }
+}
+
+export default Car
